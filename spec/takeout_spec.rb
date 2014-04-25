e3c3bb93203739de1585f0b7f8c9ab23978e6657
Robert Leon <llexi@hotmail.com>
2014-04-25 16:48:11 +0100

    Testing content of Menu hash

--- spec/takeout_spec.rb
+++ spec/takeout_spec.rb
@@ -4,9 +4,14 @@ describe Takeaway do
 
   context "menu items" do
 
-    it "will have a list of dishes on the menu" do
+    it "will have a list of dishes in a menu" do
     	takeaway = Takeaway.new
       expect(takeaway.menu.to_s).to include "somethingtasty"
+    end
+
+    it "will include prices in the menu" do
+    	takeaway = Takeaway.new
+    	expect(takeaway.menu.to_s).to include "5"
     end
 
   end  
